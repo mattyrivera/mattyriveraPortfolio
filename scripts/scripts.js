78f62c6daf356e0ef28621184c3dfa34534bd869
mattrivera <rivera182matty@gmail.com>
2018-12-16 00:49:02 -0500

    updated html, styles set-up, functionality of nav

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+
+//     $(".hamburger").on("click", function () {
+//         $(".mainNav").removeClass("closed");
+//         $(".mainNav").addClass("opened");
+//     })
+
+//     $(".close").on("click", function () {
+//         $(".mainNav").removeClass("opened");
+//         $(".mainNav").addClass("closed");
+//     })
+
+// })
